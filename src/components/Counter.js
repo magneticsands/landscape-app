0868edc86a52031704d0c3456514a1f81b8b66ed
magneticsands <46971321+magneticsands@users.noreply.github.com>
2020-11-09 16:35:45 -0500

    Counter update
    Counters don't exceed 30 pips each (can be changed)
    
    Buttons are disabled when 0 or >29.
    
    Styling fixed so buttons don't move around.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,8 @@
+import React, { useState } from "react";
 import './Components.css';
-import React, { useState } from 'react';
 import Plus from '../img/plus.svg';
-import Minus from '../img/minus.svg';
+//import Minus from '../components/Minus.js';
+//import Minus from '../img/minus.svg';
 //import { render } from 'react-dom';
 
 const Counter = (props) => {
@@ -9,19 +10,32 @@ const Counter = (props) => {
 
     return (
         <div className="counter">
-           <button className="counter-button-container" onClick={() => setCount(count - 1)}>
-                <img src={Minus} alt="Minus-Sign" className="minus-button" />
-            </button> 
+          <button disabled={count < 1} className="counter-button-container" onClick={() => setCount(count - 1)}>
+            <svg className="minus-button" width={20} height={2} viewBox="0 0 20 2" {...props}>
+        <path
+          fill=""
+          stroke=""
+          strokeLinecap="round"
+          strokeWidth={2}
+          d="M1 1h18"
+        />
+      </svg>   
+      </button>
             <span className={`value--${props.color}`}>
                {count}
                 </span>
-
-            <button className="counter-button-container" onClick={() => setCount(count + 1)}>
-              <img src={Plus} alt="Plus-Sign" className="plus-button" />
-                </button> 
+            <button disabled={count > 29} className="counter-button-container" onClick={() => setCount(count + 1)}>
+            <svg className="plus-button" width={22} height={22} viewBox="0 0 22 22" {...props}>
+      <g fill="" stroke="" strokeLinecap="round" strokeWidth={2}>
+        <path d="M11 1v20M1 11h20" />
+      </g>
+    </svg>
+           </button> 
         </div>
     );
  }
   
 export default Counter;
-  
+   //
+              //<img src={Plus} alt="Plus-Sign" className="plus-button" />
+                //
